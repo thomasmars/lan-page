bcdc7ad51a3f347125466feb3e408028d07557fc
Thomas Marstrander <thomasmars87@gmail.com>
2016-08-07 17:57:44 +0200

    Added oAuth buttons for all providers.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 import './index.html'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Layout from './layout'
+import { Button } from 'react-bootstrap'
+
 
 var horizon = Horizon({
   authType: 'token'
 })
 
+const authorize = function (identityProvider) {
+  horizon.authEndpoint(identityProvider).subscribe((endpoint) => {
+    window.location.pathname = endpoint;
+  });
+}
+
+const oAuthProviders = [
+  'facebook',
+  'twitch',
+  'google',
+  'github',
+  'twitter'
+]
+
 horizon.connect()
-ReactDOM.render(<Layout/>, document.querySelector('.app'))
+ReactDOM.render(
+  <div>
+    {oAuthProviders.map((provider) => {
+      return <Button onClick={authorize.bind(this, provider)} bsSize="large">This is the {provider} button</Button>
+    })}
+  </div>
+  , document.querySelector('.app')
+)
--- src/layout.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-
-const Layout = () => (
-  <Button bsSize="large">This is a button</Button>
-)
-
-export default Layout
