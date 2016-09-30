159247594b874ca12fd7f43107ee9064e0dde9f3
thomasmars <thomasmars87@gmail.com>
2016-09-30 22:40:33 +0200

    Simple socket communication instead of json polling

--- nodeBackend/server.js
+++ nodeBackend/server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var r = require('rethinkdb');
+var net = require('net');
 
 var app = express();
 let nextCommand = {};
@@ -11,42 +12,80 @@ app.get('/', function (req, res) {
   res.send(JSON.stringify(nextCommand));
 });
 
-app.listen(8020, function () {
-  console.log('Example app listening on port 3000!');
-});
+// app.listen(8020, function () {
+//   console.log('Example app listening on port 3000!');
+// });
 
-var connection = null;
-r.connect({
-  host: process.env.RETHINKDB_PORT_28015_TCP_ADDR,
-  port: 28015
-}, (err, conn) => {
-  if (err) throw err;
-  connection = conn;
-  console.log("hurra, established connection to db");
+// var connection = null;
+// r.connect({
+//   host: process.env.RETHINKDB_PORT_28015_TCP_ADDR,
+//   port: 28015
+// }, (err, conn) => {
+//   if (err) throw err;
+//   connection = conn;
+//   console.log("hurra, established connection to db");
+//
+//
+//   console.log("do we have any envs ??", process.env.RETHINKDB_PORT);
+//
+//   r.db('nodeThomas').table('roboQueue').run(connection, (err, result) => {
+//     if (err) {
+//       return console.log(err);
+//     }
+//
+//     result.toArray((err, arrRes) => {
+//       if (err) {
+//         console.log("err: ", err);
+//       }
+//
+//       console.log("toarray result", arrRes);
+//     })
+//   })
+//
+//   r.db('nodeThomas').table('roboQueue').changes().run(connection, (err, cursor) => {
+//     cursor.each((cErr, res) => {
+//       if (err) throw err
+//       console.log("res", res)
+//       nextCommand = res;
+//     })
+//   })
+// })
 
 
-  console.log("do we have any envs ??", process.env.RETHINKDB_PORT);
+var socketServer = net.createServer((socket) => {
+  let socketOpen = true
 
-  r.db('nodeThomas').table('roboQueue').run(connection, (err, result) => {
-    if (err) {
-      return console.log(err);
-    }
-
-    result.toArray((err, arrRes) => {
-      if (err) {
-        console.log("err: ", err);
+  const repeatSignal = () => {
+    console.log("repeating signal")
+    setTimeout(() => {
+      if (socketOpen) {
+        socket.write('command F')
+        repeatSignal();
       }
+    }, 500)
+  }
 
-      console.log("toarray result", arrRes);
-    })
+  socket.setEncoding('utf8')
+
+  socket.on('data', (data) => {
+    console.log("client says", data)
   })
 
-  r.db('nodeThomas').table('roboQueue').changes().run(connection, (err, cursor) => {
-    cursor.each((cErr, res) => {
-      if (err) throw err
-      console.log("res", res)
-      nextCommand = res;
-    })
+  socket.on('end', () => {
+    socketOpen = false
+    console.log("client disconnected!");
   })
+
+  socket.write('this is server!\r\n');
+
+  repeatSignal()
+
+  // socket.pipe(socket)
+}).on('error', (err) => {
+  throw err
+})
+
+socketServer.listen(8020, () => {
+  console.log("socket listening on 8020, right ?", socketServer.address())
 })
 
